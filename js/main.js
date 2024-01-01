0e9baf592745bdfada01cd31ee0062d294d3f3ac
Shaqs0 <hhgfhhhggb@gmail.com>
2024-01-01 16:44:52 +0300

    ss

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function clearCache() {
+    if ('caches' in window) {
+        caches.keys().then(function (cacheNames) {
+            cacheNames.forEach(function (cacheName) {
+                caches.delete(cacheName);
+            });
+        });
+    }
+
+}
+
+clearCache();
